c107cac76b64ba3a4e57c0d39583e6145cb77c37
Neylanio <neylaniogomes@gmail.com>
2020-07-30 19:05:34 -0300

    feat: validate Login
    Using @unform/core and @unform/web

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
-
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
+import * as Yup from 'yup';
+
+import { Form } from '@unform/web';
+
 import { Link } from 'react-router-dom';
+import { FormHandles } from '@unform/core';
+import getValidationErrors from '../../utils/getValidationErros';
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
@@ -10,24 +15,47 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="logo" />
-      <form>
-        <h1>Faça seu Logon</h1>
-        <Input type="text" icon={FiMail} name="email" placeholder="E-mail" autoFocus />
-        <Input type="password" icon={FiLock} name="password" placeholder="Senha" />
-        <Button type="submit">Entrar</Button>
-        <a href="#/">Esqueci minha senha</a>
-      </form>
-      <Link to="/signup">
-        <FiLogIn size={20} />
-        Criar conta
-      </Link>
-    </Content>
-    <Background />
-  </Container>
-);
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: any) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string().required('E-mail é obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha é obrigatória'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      const errors = getValidationErrors(error);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="logo" />
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu Logon</h1>
+          <Input name="email" icon={FiMail} placeholder="E-mail" autoFocus />
+          <Input type="password" icon={FiLock} name="password" placeholder="Senha" />
+          <Button type="submit">Entrar</Button>
+          <a href="#/">Esqueci minha senha</a>
+        </Form>
+        <Link to="/signup">
+          <FiLogIn size={20} />
+          Criar conta
+        </Link>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
 
 export default SignIn;
