8a80bcb943251bcb7372fd3a3b38895ef97ee13e
Neylanio <neylaniogomes@gmail.com>
2020-07-29 22:30:07 -0300

    feat: use UnForm library
    Using UnForm library to change the traditional form element

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -4,6 +4,8 @@ import {
   FiMail, FiLock, FiUser, FiArrowLeft,
 } from 'react-icons/fi';
 
+import { Form } from '@unform/web';
+
 import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/logo.svg';
@@ -13,25 +15,31 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logoImg} alt="logo" />
-      <form>
-        <h1>Faça seu cadastro</h1>
-        <Input type="text" icon={FiUser} name="name" placeholder="Nome" autoFocus />
-        <Input type="text" icon={FiMail} name="email" placeholder="E-mail" />
-        <Input type="password" icon={FiLock} name="password" placeholder="Senha" />
+const SignUp: React.FC = () => {
+  function handleSubmit(data: any): void {
+    console.log(data);
+  }
 
-        <Button type="submit">Cadastrar</Button>
-      </form>
-      <Link to="/">
-        <FiArrowLeft size={20} />
-        Voltar p/ Logon
-      </Link>
-    </Content>
-  </Container>
-);
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="logo" />
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+          <Input type="text" icon={FiUser} name="name" placeholder="Nome" autoFocus />
+          <Input type="text" icon={FiMail} name="email" placeholder="E-mail" />
+          <Input type="password" icon={FiLock} name="password" placeholder="Senha" />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+        <Link to="/">
+          <FiArrowLeft size={20} />
+          Voltar p/ Logon
+        </Link>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
