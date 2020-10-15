f729f0c875042ccd05877c6d23e65d36b069b1ca
Neylanio <neylaniogomes@gmail.com>
2020-10-15 15:17:56 -0300

    feat: Do header from dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,15 @@
 import React, { useCallback } from 'react';
+import { FiPower } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 import { useToast } from '../../hooks/toast';
 
+import logoImg from '../../assets/logo.svg';
+import {
+  Container, Header, HeaderContent, Profile,
+} from './styles';
+
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
   const { addToast } = useToast();
 
   const logout = useCallback(() => {
@@ -15,12 +21,25 @@ const Dashboard: React.FC = () => {
   }, [signOut, addToast]);
 
   return (
-    <>
-      <strong>Dashboard</strong>
-      <button type="button" onClick={logout}>
-        Logout
-      </button>
-    </>
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem vindo, </span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={logout}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
   );
 };
 
